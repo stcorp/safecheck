98f05cdcc524ad6be07b69571a6bcec53bb217cf
Hajduch Guillaume <ghajduch@groupcls.com>
2023-05-03 20:43:49 +0200

    support S1 COG products

--- safecheck.py
+++ safecheck.py
@@ -30,272 +30,7 @@ included in the manifest file are also performed.
 
 NSXFDU = '{urn:ccsds:schema:xfdu:1}'
 
-builtin_manifest_schema = """<?xml version="1.0"?>
-<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xfdu="urn:ccsds:schema:xfdu:1"
-    targetNamespace="urn:ccsds:schema:xfdu:1" elementFormDefault="unqualified" attributeFormDefault="unqualified">
-  <xs:simpleType name="locatorTypeType">
-    <xs:restriction base="xs:string">
-      <xs:enumeration value="URL"/>
-      <xs:enumeration value="OTHER"/>
-    </xs:restriction>
-  </xs:simpleType>
-  <xs:simpleType name="otherLocatorTypeType">
-    <xs:restriction base="xs:string"/>
-  </xs:simpleType>
-  <xs:attributeGroup name="LOCATION">
-    <xs:attribute name="locatorType" use="required" type="xfdu:locatorTypeType"/>
-    <xs:attribute name="otherLocatorType" type="xfdu:otherLocatorTypeType"/>
-  </xs:attributeGroup>
-  <xs:attributeGroup name="registrationGroup">
-    <xs:attribute name="registrationAuthority" type="xs:string" use="optional"/>
-    <xs:attribute name="registeredID" type="xs:string" use="optional"/>
-  </xs:attributeGroup>
-  <xs:simpleType name="vocabularyNameType">
-    <xs:restriction base="xs:string"/>
-  </xs:simpleType>
-  <xs:simpleType name="versionType">
-    <xs:restriction base="xs:string"/>
-  </xs:simpleType>
-  <xs:simpleType name="mimeTypeType">
-    <xs:restriction base="xs:string"/>
-  </xs:simpleType>
-  <xs:simpleType name="checksumNameType">
-    <xs:restriction base="xs:string">
-      <xs:enumeration value="MD5"/>
-      <xs:enumeration value="CRC32"/>
-    </xs:restriction>
-  </xs:simpleType>
-  <xs:simpleType name="combinationMethodType">
-    <xs:restriction base="xs:string">
-      <xs:enumeration value="concat"/>
-    </xs:restriction>
-  </xs:simpleType>
-  <xs:attribute name="namespace" type="xs:string"/>
-  <xs:complexType name="referenceType">
-    <xs:sequence/>
-    <xs:attribute name="href" type="xs:string" use="required"/>
-    <xs:attribute name="ID" type="xs:ID"/>
-    <xs:attribute name="textInfo" type="xs:string"/>
-    <xs:attributeGroup ref="xfdu:LOCATION"/>
-    <xs:attribute name="locator" type="xs:string" use="optional" default="/"/>
-  </xs:complexType>
-  <xs:complexType name="checksumInformationType">
-    <xs:simpleContent>
-      <xs:extension base="xs:string">
-        <xs:attribute name="checksumName" type="xfdu:checksumNameType" use="required"/>
-      </xs:extension>
-    </xs:simpleContent>
-  </xs:complexType>
-  <xs:complexType name="metadataObjectType">
-    <xs:sequence>
-      <xs:element name="metadataReference" type="xfdu:metadataReferenceType" minOccurs="0"/>
-      <xs:element name="metadataWrap" type="xfdu:metadataWrapType" minOccurs="0"/>
-      <xs:element name="dataObjectPointer" type="xfdu:dataObjectPointerType" minOccurs="0"/>
-    </xs:sequence>
-    <xs:attribute name="ID" use="required">
-      <xs:simpleType>
-        <xs:restriction base="xs:ID">
-          <xs:pattern value="processing"/>
-          <xs:pattern value="(a|.+A)cquisitionPeriod"/>
-          <xs:pattern value="(p|.+P)latform"/>
-          <xs:pattern value=".+Schema"/>
-          <xs:pattern value=".+QualityInformation"/>
-          <xs:pattern value=".+OrbitReference"/>
-          <xs:pattern value=".+GridReference"/>
-          <xs:pattern value=".+FrameSet"/>
-          <xs:pattern value=".+Index"/>
-          <xs:pattern value=".+Annotation"/>
-          <xs:pattern value=".+Information"/>
-        </xs:restriction>
-      </xs:simpleType>
-    </xs:attribute>
-    <xs:attribute name="classification">
-      <xs:simpleType>
-        <xs:restriction base="xs:string">
-          <xs:enumeration value="DED"/>
-          <xs:enumeration value="SYNTAX"/>
-          <xs:enumeration value="FIXITY"/>
-          <xs:enumeration value="PROVENANCE"/>
-          <xs:enumeration value="CONTEXT"/>
-          <xs:enumeration value="REFERENCE"/>
-          <xs:enumeration value="DESCRIPTION"/>
-          <xs:enumeration value="OTHER"/>
-        </xs:restriction>
-      </xs:simpleType>
-    </xs:attribute>
-    <xs:attribute name="category">
-      <xs:simpleType>
-        <xs:restriction base="xs:string">
-          <xs:enumeration value="REP"/>
-          <xs:enumeration value="PDI"/>
-          <xs:enumeration value="DMD"/>
-          <xs:enumeration value="OTHER"/>
-          <xs:enumeration value="ANY"/>
-        </xs:restriction>
-      </xs:simpleType>
-    </xs:attribute>
-  </xs:complexType>
-  <xs:complexType name="metadataReferenceType">
-    <xs:sequence/>
-    <xs:attribute name="href" type="xs:string" use="required"/>
-    <xs:attribute name="ID" type="xs:ID"/>
-    <xs:attribute name="textInfo" type="xs:string"/>
-    <xs:attributeGroup ref="xfdu:LOCATION"/>
-    <xs:attribute name="locator" type="xs:string" use="optional" default="/"/>
-    <xs:attribute name="vocabularyName" type="xfdu:vocabularyNameType"/>
-    <xs:attribute name="mimeType" type="xfdu:mimeTypeType"/>
-  </xs:complexType>
-  <xs:complexType name="xmlDataType">
-    <xs:sequence>
-      <xs:any namespace="##any" processContents="lax" maxOccurs="unbounded"/>
-    </xs:sequence>
-  </xs:complexType>
-  <xs:complexType name="fileContentType">
-    <xs:choice>
-      <xs:element name="binaryData" type="xs:base64Binary" minOccurs="0"/>
-      <xs:element name="xmlData" type="xfdu:xmlDataType" minOccurs="0"/>
-    </xs:choice>
-    <xs:attribute name="ID" type="xs:ID"/>
-  </xs:complexType>
-  <xs:complexType name="metadataWrapType">
-    <xs:sequence>
-      <xs:element name="xmlData" type="xfdu:xmlDataType"/>
-    </xs:sequence>
-    <xs:attribute name="mimeType" type="xfdu:mimeTypeType"/>
-    <xs:attribute name="textInfo" type="xs:string"/>
-    <xs:attribute name="vocabularyName" type="xfdu:vocabularyNameType"/>
-  </xs:complexType>
-  <xs:complexType name="dataObjectPointerType">
-    <xs:attribute name="ID" type="xs:ID"/>
-    <xs:attribute name="dataObjectID" use="required" type="xs:IDREF"/>
-  </xs:complexType>
-  <xs:complexType name="keyDerivationType">
-    <xs:attribute name="name" use="required" type="xs:string"/>
-    <xs:attribute name="salt" use="required">
-      <xs:simpleType>
-        <xs:restriction base="xs:string">
-          <xs:length value="16"/>
-        </xs:restriction>
-      </xs:simpleType>
-    </xs:attribute>
-    <xs:attribute name="iterationCount" use="required" type="xs:long"/>
-  </xs:complexType>
-  <xs:element name="abstractKeyDerivation" type="xfdu:keyDerivationType" abstract="true"/>
-  <xs:element name="keyDerivation" type="xfdu:keyDerivationType" substitutionGroup="xfdu:abstractKeyDerivation"/>
-  <xs:complexType name="transformObjectType">
-    <xs:sequence>
-      <xs:element name="algorithm" type="xs:string"/>
-      <xs:element ref="xfdu:abstractKeyDerivation" minOccurs="0" maxOccurs="unbounded"/>
-    </xs:sequence>
-    <xs:attribute name="ID" type="xs:ID"/>
-    <xs:attribute name="order" type="xs:string"/>
-    <xs:attribute name="transformType" use="required">
-      <xs:simpleType>
-        <xs:restriction base="xs:string">
-          <xs:enumeration value="COMPRESSION"/>
-          <xs:enumeration value="AUTHENTICATION"/>
-          <xs:enumeration value="ENCRYPTION"/>
-        </xs:restriction>
-      </xs:simpleType>
-    </xs:attribute>
-  </xs:complexType>
-  <xs:complexType name="byteStreamType">
-    <xs:sequence>
-      <xs:element name="fileLocation" type="xfdu:referenceType"/>
-      <xs:element name="checksum" type="xfdu:checksumInformationType"/>
-      <!-- start: L0 specific -->
-      <xs:element name="byteOrder" minOccurs="0">
-        <xs:simpleType>
-          <xs:restriction base="xs:string">
-            <xs:enumeration value="LITTLE_ENDIAN"/>
-            <xs:enumeration value="BIG_ENDIAN"/>
-          </xs:restriction>
-        </xs:simpleType>
-      </xs:element>
-      <xs:element name="averageBitRate" type="xs:long" minOccurs="0"/>
-      <!-- end: L0 specific -->
-    </xs:sequence>
-    <xs:attribute name="ID" use="optional" type="xs:ID"/>
-    <xs:attribute name="mimeType" type="xfdu:mimeTypeType" use="required"/>
-    <xs:attribute name="size" type="xs:long"/>
-  </xs:complexType>
-  <xs:complexType name="dataObjectType">
-    <xs:sequence>
-      <xs:element name="byteStream" type="xfdu:byteStreamType" maxOccurs="unbounded"/>
-    </xs:sequence>
-    <xs:attribute name="ID" type="xs:ID" use="required"/>
-    <xs:attribute name="repID" type="xs:IDREFS" use="required"/>
-    <xs:attribute name="size" type="xs:long"/>
-    <xs:attribute name="combinationName" type="xfdu:combinationMethodType" use="optional"/>
-    <xs:attributeGroup ref="xfdu:registrationGroup"/>
-  </xs:complexType>
-  <xs:complexType name="dataObjectSectionType">
-    <xs:sequence>
-      <xs:element name="dataObject" type="xfdu:dataObjectType" maxOccurs="unbounded"/>
-    </xs:sequence>
-  </xs:complexType>
-  <xs:complexType name="contentUnitType">
-    <xs:sequence>
-      <xs:element name="dataObjectPointer" type="xfdu:dataObjectPointerType" minOccurs="0"/>
-      <xs:element ref="xfdu:abstractContentUnit" minOccurs="0" maxOccurs="unbounded"/>
-    </xs:sequence>
-    <xs:attribute name="ID" type="xs:ID" use="optional"/>
-    <xs:attribute name="order" type="xs:string"/>
-    <xs:attribute name="unitType" type="xs:string"/>
-    <xs:attribute name="textInfo" type="xs:string"/>
-    <xs:attribute name="repID" type="xs:IDREFS"/>
-    <xs:attribute name="dmdID" type="xs:IDREFS"/>
-    <xs:attribute name="pdiID" type="xs:IDREFS"/>
-    <xs:attribute name="anyMdID" type="xs:IDREFS"/>
-    <xs:attribute name="behaviorID" type="xs:IDREF"/>
-    <xs:anyAttribute namespace="##other" processContents="lax"/>
-  </xs:complexType>
-  <xs:element name="abstractContentUnit" type="xfdu:contentUnitType" abstract="true"/>
-  <xs:element name="contentUnit" type="xfdu:contentUnitType" substitutionGroup="xfdu:abstractContentUnit"/>
-  <xs:complexType name="informationPackageMapType">
-    <xs:sequence>
-      <xs:element ref="xfdu:abstractContentUnit"/>
-    </xs:sequence>
-  </xs:complexType>
-  <xs:complexType name="interfaceDefinitionType">
-    <xs:complexContent>
-      <xs:extension base="xfdu:referenceType">
-        <xs:sequence>
-          <xs:element name="inputParameter" minOccurs="0" maxOccurs="unbounded">
-            <xs:complexType mixed="true">
-              <xs:sequence>
-                <xs:element name="dataObjectPointer" type="xfdu:dataObjectPointerType" minOccurs="0"/>
-              </xs:sequence>
-              <xs:attribute name="name" use="required" type="xs:string"/>
-              <xs:attribute name="value" type="xs:string"/>
-            </xs:complexType>
-          </xs:element>
-        </xs:sequence>
-      </xs:extension>
-    </xs:complexContent>
-  </xs:complexType>
-  <xs:element name="abstractMechanism" type="xfdu:mechanismType" abstract="true"/>
-  <xs:complexType name="mechanismType">
-    <xs:complexContent>
-      <xs:extension base="xfdu:referenceType"/>
-    </xs:complexContent>
-  </xs:complexType>
-  <xs:complexType name="metadataSectionType">
-    <xs:sequence>
-      <xs:element name="metadataObject" type="xfdu:metadataObjectType" minOccurs="2" maxOccurs="unbounded"/>
-    </xs:sequence>
-  </xs:complexType>
-  <xs:complexType name="XFDUType">
-    <xs:sequence>
-      <xs:element name="informationPackageMap" type="xfdu:informationPackageMapType"/>
-      <xs:element name="metadataSection" type="xfdu:metadataSectionType" minOccurs="0"/>
-      <xs:element name="dataObjectSection" type="xfdu:dataObjectSectionType" minOccurs="0"/>
-    </xs:sequence>
-    <xs:attribute name="version" type="xfdu:versionType" use="required"/>
-  </xs:complexType>
-  <xs:element name="XFDU" type="xfdu:XFDUType"/>
-</xs:schema>
-"""
+builtin_manifest_schema = """"""
 
 
 def check_file_against_schema(xmlfile, schema):
@@ -339,18 +74,31 @@ def md5sum(filename):
     return md5.hexdigest()
 
 
-def check_product_crc(product, manifestfile):
+def s1_check_product_crc(product, manifestfile):
+    """
+    Check CRC part of the product name.
+    Only applies to S1 products
+    """
     expected_crc = format(binascii.crc_hqx(pathlib.Path(manifestfile).read_bytes(), 0xFFFF), '04X')
-    actual_crc = pathlib.Path(product).stem[-4:]
+    # Standard products the CRC is the last 4 chararcters in the productname, before the .SAFE extension
+    # This is as well the 9th subpart of product name while splitting using '_'
+    # On GRD/COG products, an additional '_COG' is added in product name
+    actual_crc = pathlib.Path(product).stem.split('_')[8]
     if expected_crc != actual_crc:
         logger.warning(f"crc in product name '{actual_crc}' does not match crc of manifest file '{expected_crc}'")
         return False
     return True
 
 
-def check_manifest_file(file, schema=None):
+def check_manifest_file(file, schema=None, mission=None):
     if schema is None:
-        schema = builtin_manifest_schema
+        if mission == 'S1':
+            xsd_name = 's1_buildin_manifest.xsd'
+            schema = os.path.join(os.path.dirname(__file__), 'xsd', xsd_name)
+
+        elif mission == 'S2':
+            xsd_name = 's2_buildin_manifest.xsd'
+            schema = '/home/ghajduch/Documents/2023/2023-03/S2/S2-PDGS-TAS-DI-PSD-V14.9_SAFE/resources/xsd/int/esa/safe/sentinel/1.1/xfdu.xsd'
     return check_file_against_schema(file, schema)
 
 
@@ -359,6 +107,8 @@ def verify_safe_product(product, manifest_schema=None):
     has_warnings = False
 
     product = pathlib.Path(product)
+
+    mission = product.name[0:2]
 
     if not product.exists():
         logger.error(f"could not find '{product}'")
@@ -369,11 +119,11 @@ def verify_safe_product(product, manifest_schema=None):
         logger.error(f"could not find '{manifestfile}'")
         return 2
 
-    if product.name[4:7] != "AUX":
-        if not check_product_crc(product, manifestfile):
+    if mission == "S1" and product.name[4:7] != "AUX":
+        if not s1_check_product_crc(product, manifestfile):
             has_warnings = True
 
-    if not check_manifest_file(manifestfile, manifest_schema):
+    if not check_manifest_file(manifestfile, manifest_schema, mission=mission):
         has_errors = True
     manifest = etree.parse(os.fspath(manifestfile))
     if manifest is None:
@@ -399,17 +149,18 @@ def verify_safe_product(product, manifest_schema=None):
             if filepath in files:
                 files.remove(filepath)
 
-    information_package_map = manifest.find('informationPackageMap')
-    for content_unit in information_package_map.findall(f'{NSXFDU}contentUnit/{NSXFDU}contentUnit'):
-        data_object_id = content_unit.find('dataObjectPointer').get('dataObjectID')
-        rep_id = content_unit.get('repID')
-        # rep_id can be a space separated list of IDs (first one contains the main schema)
-        rep_id = rep_id.split()[0]
-        if rep_id not in reps:
-            logger.error(f"dataObject '{data_object_id}' in informationPackageMap contains repID '{rep_id}' which "
-                         f"is not defined in metadataSection")
-            return 2
-        data_objects[data_object_id] = {'rep': reps[rep_id]}
+    if mission == 'S1':
+        information_package_map = manifest.find('informationPackageMap')
+        for content_unit in information_package_map.findall(f'{NSXFDU}contentUnit/{NSXFDU}contentUnit'):
+            data_object_id = content_unit.find('dataObjectPointer').get('dataObjectID')
+            rep_id = content_unit.get('repID')
+            # rep_id can be a space separated list of IDs (first one contains the main schema)
+            rep_id = rep_id.split()[0]
+            if rep_id not in reps:
+                logger.error(f"dataObject '{data_object_id}' in informationPackageMap contains repID '{rep_id}' which "
+                             f"is not defined in metadataSection")
+                return 2
+            data_objects[data_object_id] = {'rep': reps[rep_id]}
 
     data_object_section = manifest.find('dataObjectSection')
     for data_object in data_object_section.findall('dataObject'):
